390ece264ddd0e76f6fe58ef8e8b660f1722fd9d
SahilChaudhary17 <sahilch38@gmail.com>
2021-10-12 18:54:43 +0530

    guess the number

---
+++ guess the number.py
@@ -0,0 +1,23 @@
+import random
+num = random.randint(1, 9)
+player_name = input("Hello mate, What's your name ?\n")
+guess_count = 0
+print("welcome ", player_name, ", are you ready to plat the guessing game ? (yes/no)")
+a = input()
+if a == "yes":
+    print("I'm guessing a number between 1 to 9")
+    while guess_count < 5:
+        guess = int(input())
+        guess_count += 1
+        if guess < num:
+            print('Your guess is too low')
+        if guess > num:
+            print('Your guess is too high')
+        if guess == num:
+            print("Hurray, you guessed it right!")
+        if guess == num:
+            print('You guessed the number in ' + str(guess_count) + ' tries!')
+        else:
+            print('You did not guess the number, The number was ' + str(num))
+else:
+    print("hoping to see you soon again! :)")
