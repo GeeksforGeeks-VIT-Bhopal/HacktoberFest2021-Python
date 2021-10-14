1fb81524a8a346c9ae45b033fb8da13e8696d7f3
TirthPinkalSheth <tirthsheth100304@gmail.com>
2021-10-14 14:50:21 +0530

    A strong password generator is made

--- strongpasswordgenerator.py
+++ strongpasswordgenerator.py
@@ -1,4 +1,6 @@
 #strong and nearly uncrackable password generator
 a= input ("enter first name with first letter as capital")
 b= input ("enter date of birth as DDMMYYYY")
-password= print(a[0:2]+"@"+b)# concatenation is used 
+password= print(a[0:2]+"@"+b)
+# concatenation is used 
+print("password is ", password)
