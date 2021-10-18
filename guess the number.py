819bae4288329cd118d85c0443dd2055eb17e00d
Sahil8470 <91491044+Sahil8470@users.noreply.github.com>
2021-10-19 00:45:34 +0530

    Guess_game-Sahil8470 (#47)

--- guess the number.py
+++ guess the number.py
@@ -1,22 +1,38 @@
 import random
-num = random.randint(1, 9)
-player_name = input("Hello mate, What's your name ?\n")
+player_name1 = input("Hello player 1, What's your name?\n")
+player_name2 = input("Hello player 2, What's your name?\n")
 guess_count = 0
-print("welcome ", player_name, ", are you ready to plat the guessing game ? (yes/no)"), a = input()
-if a == "yes":
-    print("I'm guessing a number between 1 to 9")
-    while guess_count < 5:
-        guess = int(input())
+print("welcome ", player_name1, "and", player_name2, ",this is the number guessing game.")
+p1_points = 0
+p2_points = 0
+while True:
+    num = random.randint(1, 9)
+    print(player_name1, "type your guess.")
+    g1 = int(input())
+    print(player_name2, "type your guess.")
+    g2 = int(input())
+    if g1 == num:
+        print("Hurray", player_name1, ",you guessed it right!")
+        p1_points += 1
+    else:
+        print("Sorry", player_name1, ',you guessed it wrong.')
+    if g2 == num:
+        print("Hurray", player_name2, ",you guessed it right!")
+        p2_points += 1
+    else:
+        print("Sorry", player_name2, ',you guessed it wrong.')
         guess_count += 1
-        if guess < num:
-            print('Your guess is too low')
-        if guess > num:
-            print('Your guess is too high')
-        if guess == num:
-            print("Hurray, you guessed it right!")
-            print('You guessed the number in ' + str(guess_count) + ' tries!')
-        break
+        print("The right number is", num, ".")
+    cont = input("Do you both wish to play again? If yes then type 'yes'.\n")
+
+    if cont != 'yes':
+        print('Thank you for playing.')
+        print(player_name1, ", you had", p1_points, "right out of", guess_count, ".")
+        print(player_name2, ", you had", p2_points, "right out of", guess_count, ".")
+        if p1_points > p2_points:
+            print(player_name1, ", you won the game.")
+        elif p1_points < p2_points:
+            print(player_name2, ", you won the game.")
         else:
-            print('You did not guess the number, The number was ' + str(num))
-else:
-    print("hoping to see you soon again! :)")
+            print("Both have same points, so that's a tie.")
+        break
